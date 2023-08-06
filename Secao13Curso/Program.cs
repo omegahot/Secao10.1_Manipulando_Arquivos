95572aa6ccbf508f8a91317d73c71ad87df5b357
Leonardo <leo_omegahot@hotmail.com>
2023-08-06 20:20:09 -0300

    Criando uma cópia de um arquivo e lendo o conteúdo do arquivo.

--- Secao13Curso/Program.cs
+++ Secao13Curso/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        string sourcePath = @"C:\Temporarios\arquivos\file1.txt";
+        string targetPath = @"C:\Temporarios\arquivos\file2.txt";
+
+        try
+        {
+            FileInfo fileInfo = new FileInfo(sourcePath);
+            // fileInfo.CopyTo(targetPath);
+            string[] lines = File.ReadAllLines(sourcePath);
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Um erro ocorreu!");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
