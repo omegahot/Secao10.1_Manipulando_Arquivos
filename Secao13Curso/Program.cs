d9d0cd71170902a7b4975a6254f75751eac2dc92
Leonardo <leo_omegahot@hotmail.com>
2023-08-06 21:41:43 -0300

    Utilizando a função Path.

--- Secao13Curso/Program.cs
+++ Secao13Curso/Program.cs
@@ -1,39 +1,12 @@
 ﻿using System.IO;
-using System.Collections.Generic;
 
 class Program
 {
     private static void Main(string[] args)
     {
-        string path = @"C:\Temporarios\arquivos";
-
-        try
-        {
-            // Listar todos os diretórios.
-            IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
-            Console.WriteLine("DIRECTORIES: ");
-
-            foreach(string folder in folders)
-            {
-                Console.WriteLine(folder);
-            }
-
-            //Listar todos os arquivos.
-            Console.WriteLine();
-            Console.WriteLine("Listando todos os arquivos:");
-            var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
-            foreach (string file in files)
-            {
-                Console.WriteLine(file);
-            }
-
-            //Criando um diretório
-            Directory.CreateDirectory(path + @"\new_folder");
-        }
-        catch (IOException e)
-        {
-            Console.WriteLine("Um erro ocorreu.");
-            Console.WriteLine(e.Message);
-        }
+        string path = @"C:\Temporarios\arquivos\file1.txt";
+        Console.WriteLine(Path.GetDirectoryName(path));
+        Console.WriteLine(Path.GetFileName(path));
+        Console.WriteLine(Path.DirectorySeparatorChar);
     }
 }
